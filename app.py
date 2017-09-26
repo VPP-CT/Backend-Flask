0da3f3901941c5a9535b4eca8b78bb793dee49b9
brokenmouse <youxiang518@gmail.com>
2017-09-26 16:43:29 -0400

    Add TODO to app.py file
    Add TODOs descriptions to app.py file.
    
    Please take a look on those comments in the source code.

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from flask import Flask, request
 from google_flight import google_flight_api
 
 app = Flask(__name__)
-qpx = google_flight_api.GoogleFlight(open("apikey.key", "r").read())  # Google QPX lib
+
+# Google QPX lib
+qpx = google_flight_api.GoogleFlight(open("apikey.key", "r").read())
 
 
 @app.route('/')
@@ -17,13 +19,20 @@ def index():
 
 @app.route('/flights')
 def flights():
+    # initialize data from RESTful call by construct a flight_data_obj
+    #
+    # TODO: create a constructor which could dynamically construct the
+    # flight_data_obj by the given segment number. Each segment should contain
+    # a orig, dest, and date.
     flight_data = flight_data_obj()
     flight_data.budget = request.args.get('budget')
     flight_data.seg = request.args.get('seg')
     flight_data.orig1 = request.args.get('origin1')
     flight_data.dest1 = request.args.get('dest1')
     flight_data.date1 = request.args.get('date1')
+    # end TODO
 
+    # TODO: change the logging format accordingly based on the constructor.
     logging.warning(
         "Budget:%s, Segment: %s\nOrigin1:%s, Destination1:%s, Date1:%s",
         flight_data.budget,
@@ -31,11 +40,17 @@ def flights():
         flight_data.orig1,
         flight_data.dest1,
         flight_data.date1)
+    # end TODO
 
     data = {
         "request": {
             "slice": [
-                dict(origin=flight_data.orig1, destination=flight_data.dest1, date=flight_data.date1),
+                # TODO: based on the len(constructed_data), add each segment
+                # into the request body as dict.
+                dict(origin=flight_data.orig1,
+                     destination=flight_data.dest1,
+                     date=flight_data.date1),
+                # end TODO
             ],
             "passengers": dict(adultCount=1),
             "refundable": 'false'
@@ -44,7 +59,7 @@ def flights():
 
     get_flights(data)
 
-    return "/flights/?budget=xxx&seg=x&origin1=xxx&dest1=xxx&date1=xxxx-xx-xx"
+    return "/flights?budget=xxx&seg=x&origin1=xxx&dest1=xxx&date1=xxxx-xx-xx"
 
 
 def get_flights(raw_input):
@@ -52,6 +67,12 @@ def get_flights(raw_input):
 
     logging.warning(qpx.data)
     print(qpx.data, file=open('latest_query.log', 'w+'))
+
+    # TODO: re-format the data into JSON format. Remove all unnecessary
+    # fields, and append a ranking field into it (now we could use price to
+    # rank them.)
+    return qpx.data
+    # end TODO
 
 
 class flight_data_obj(object):
