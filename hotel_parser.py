7ba931e76d01a1725e1a6e94e25e84a5b968c4a5
Misaki <misaki@dhcp-nyc-479.redrover.cornell.edu>
2017-10-11 13:50:06 -0400

    hotel data parser

--- hotel_parser.py
+++ hotel_parser.py
@@ -8,6 +8,10 @@ from __future__ import print_function
 import requests
 import logging
 import json
+# from pygeocoder import Geocoder
+import geocoder
+
+returnedNum = 5  # only return top 50 flight data
 
 
 def search_hotels(location):
@@ -24,16 +28,22 @@ def search_hotels(location):
     """
     # TODO: The Expedia API only allows location as latitude and longitude input
     # Find a way to convert location str input into the GEO data.
+    print ("HIIIIIII")
+    import geocoder
+    g = geocoder.google('Mountain View, CA')
+
     param = {'sortOrder': 'price',
              'pageIndex': 0,
              'enableSponsoredListings': 'false',
              'room1': 1,
              'filterUnavailable': 'true',
-             'checkInDate': '2017-09-30',
-             'checkOutDate': '2017-10-01',
-             'latitude': '35.675',  # location
-             'longitude': '139.76',  # location
-             'resultsPerPage': '200'}
+             'checkInDate': '2017-10-13',
+             'checkOutDate': '2017-10-14',
+             # 'latitude': '40.7128',
+             # 'longitude': '74.0060',
+             'latitude': g.latlng[0],  # location
+             'longitude': g.latlng[1],  # location
+             'resultsPerPage': returnedNum}
 
     raw_response = requests.get("https://www.expedia.com/m/api/hotel/search/v3",
                                 params=param)
@@ -54,7 +64,29 @@ def parse_hotel(raw_data, location):
 
     """
     # TODO: Parse the data, make sure it is ready for constructing package.
-    logging.warning(raw_data)
+    return_data = dict()
+    return_data['availableHotelCount'] = raw_data['availableHotelCount']
+    return_data['numberOfRoomsRequested'] = raw_data['numberOfRoomsRequested']
+    hotel_list = dict()
+    for x in range(returnedNum):
+            hotel = dict()
+            hotel['country'] = raw_data['hotelList'][x]['countryName']
+            hotel['city'] = raw_data['hotelList'][x]['city']
+            hotel['name'] = raw_data['hotelList'][x]['localizedName']
+            hotel['hotelId'] = raw_data['hotelList'][x]['hotelId']
+            # hotel['locationId'] = raw_data['hotelList'][x]['locationId']
+            hotel['address'] = raw_data['hotelList'][x]['address']
+            hotel['description'] = raw_data['hotelList'][x]['shortDescription']
+            hotel['availability'] =raw_data['hotelList'][x]['isHotelAvailable']
+            hotel['roomsLeft'] = raw_data['hotelList'][x]['roomsLeftAtThisRate']
+            hotel['totalReviews'] = raw_data['hotelList'][x]['totalReviews']
+            hotel['guestRating'] = raw_data['hotelList'][x]['hotelGuestRating']
+            hotel['starRating'] = raw_data['hotelList'][x]['hotelStarRating']
+            hotel_list['hotel%d' % x] = hotel
+    return_data['hotel_list'] = hotel_list
+    formated_return_data = json.dumps(return_data, sort_keys=True, indent=4, separators=(',', ': '))
+    print(formated_return_data, file=open('top_query_hotels.log', 'w+'))
+    # logging.warning(raw_data)
 
 
 if __name__ == '__main__':
